8ae96853c512f1a13387f26c78e4aaf17eb3b54c
Arun <arunmundakkal003@gmail.com>
2024-12-28 20:07:06 +0530

    other files

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
+
 function createSnowflake() {
     const snowflake = document.createElement('div');
-    snowflake.classList.add('snowflake');
+    snowflake.classList.add('snow');
     snowflake.style.left = Math.random() * 100 + 'vw';
     snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
-    snowflake.style.opacity = Math.random();
-    snowflake.style.width = snowflake.style.height = Math.random() * 5 + 5 + 'px';
-    
+    snowflake.innerHTML = '❄';
     document.body.appendChild(snowflake);
     
     setTimeout(() => {
         snowflake.remove();
     }, 5000);
 }
-
-setInterval(createSnowflake, 100);
-
+       
+setInterval(createSnowflake, 300);
